1c3fcab3f2cfdd1d6573ddeb8cd494e5688c9698
hstaudacher <holger.staudacher@gmail.com>
2015-11-17 19:17:44 +0100

    Add Java 8 Koan Suite

---
+++ src/test/java/com/tasktop/koans/java8/Java8KoansSuite.java
@@ -0,0 +1,14 @@
+package com.tasktop.koans.java8;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.tasktop.koans.KoanSuiteRunner;
+
+@RunWith( KoanSuiteRunner.class )
+@SuiteClasses( {
+	
+})
+public class Java8KoansSuite {
+
+}
